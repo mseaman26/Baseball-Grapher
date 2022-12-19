52a90b955cb6c9d6fd0c6cfb41b784883efbde56
michael seaman <mseaman26@gmail.com>
2022-12-19 11:02:58 -0800

    progress with web scraping

--- client/web_scraping/scraping.js
+++ client/web_scraping/scraping.js
@@ -1,9 +1,14 @@
 const cheerio = require('cheerio')
 const request = require('request')
-const mlbURL = 'https://www.mlb.com/scores/2022-04-01'
+const mlbURL = 'https://www.mlb.com/scores/2022-04-07'
 
 request(mlbURL, function (err, res, html){
     if(!err && res.statusCode == 200){
-        
+        var $ = cheerio.load(html)
+        var results = []
+        $('div.ScoresGamestyle__PaddingWrapper-sc-7t80if-5').each(function(i,element) {
+            var title = $(element).text()
+            console.log(title)
+        })
     }
 })
