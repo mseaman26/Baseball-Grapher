87cf69e3e976898cfce9c4741e69c412bf933d0b
michael seaman <mseaman26@gmail.com>
2022-12-20 11:53:49 -0800

    avoiding games being double listed and eliminating unwanted divs

--- client/web_scraping/scraping.js
+++ client/web_scraping/scraping.js
@@ -5,13 +5,23 @@ const mlbURL = 'https://www.baseball-reference.com/leagues/majors/2022-schedule.
 request(mlbURL, function (err, res, html){
     if(!err && res.statusCode == 200){
         var $ = cheerio.load(html)
-        var results = []
-        $('p.game').each(function(i,element) {
-            if(i > 0){
-                return
+        var re
+        sults = []
+        $('div').each(function(i,element) {
+            // if(i > 50){
+            //     return
+            // }
+            let day = $(element).text().replace(/\s\s+/g, '')
+            if(day.startsWith('Monday') ||day.startsWith('Tuesday') || day.startsWith('Wednesday') || day.startsWith('Thursday') || day.startsWith('Friday') || day.startsWith('Saturday') || day.startsWith('Sunday')){
+                day = day.split('Boxscore')
+                if(day.length < 20){
+                    console.log(day[0])
+                }
+                
             }
-            const day = $(element).parent().text().replace(/\s\s+/g, '').split('Boxscore')
-            console.log(day)
+            
+                // .replace(/\s\s+/g, '').split('Boxscore')
+            
         })
     }
 })
