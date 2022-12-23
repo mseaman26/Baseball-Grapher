4aada649dc5a00fd573d3f863442c444be43d2c9
michael seaman <mseaman26@gmail.com>
2022-12-22 23:51:05 -0800

    just getting used to react again

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// import Home from './pages/Home';
-// import Signup from './pages/Signup';
-// import Login from './pages/Login';
-// import SingleThought from './pages/SingleThought';
-// import Profile from './pages/Profile';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
+import Graph from './components/Graph'
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
@@ -46,9 +40,10 @@ function App() {
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
         //header would go here
+          <Graph/>
           <div className="container">
             <Routes>
-            
+                
             </Routes>
           </div>
 
---
+++ client/src/components/Graph.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+
+const Graph = ()=>{
+    return(
+        <h1>
+            hello
+        </h1>
+    )
+}
+
+export default Graph
+
