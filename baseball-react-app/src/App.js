1b8909394d96c1ef4a3c1fbd1cdb2d5f56b45f29
michael seaman <mseaman26@gmail.com>
2023-01-01 21:20:05 -0800

    messing around with canvas

--- baseball-react-app/src/App.js
+++ baseball-react-app/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+var canvas = document.getElementById("myCanvas"),
+    ctx = canvas.getContext("2d"),
+    rects = [
+        {x: 10, y: 10, w: 200, h: 50},
+        {x: 50, y: 70, w: 150, h: 30}    // etc.
+    ], i = 0, r;
+
+// render initial rects.
+while(r = rects[i++]) ctx.rect(r.x, r.y, r.w, r.h);
+ctx.fillStyle = "blue"; ctx.fill();
+
+canvas.onmousemove = function(e) {
+
+  // important: correct mouse position:
+  var rect = this.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top,
+      i = 0, r;
+  
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // for demo
+   
+  while(r = rects[i++]) {
+    // add a single rect to path:
+    ctx.beginPath();
+    ctx.rect(r.x, r.y, r.w, r.h);    
+    
+    // check if we hover it, fill red, if not fill it blue
+    ctx.fillStyle = ctx.isPointInPath(x, y) ? "red" : "blue";
+    ctx.fill();
+  }
+
+};
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    <h1>hello</h1>
+  </div>
 }
 
 export default App;
---
+++ baseball-react-app/src/drawing.js
@@ -0,0 +1,3 @@
+i
+
+console.log('poop')
