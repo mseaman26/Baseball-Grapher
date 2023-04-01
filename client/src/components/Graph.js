ef3ab35f75bc927600882d391278fe53ff841793
michael seaman <mseaman26@gmail.com>
2023-03-31 22:53:04 -0700

    got rid of webscraping inside react

--- client/src/components/Graph.js
+++ client/src/components/Graph.js
@@ -1,33 +1,12 @@
 import React, {useRef, useEffect} from "react";
-const cheerio = require('cheerio')
-const fetch = require('node-fetch')
-// import {scrapeGiantsGames} from '../webScraping/webScraping'
+
 
 const Graph = () => {
-
-  // scrapeGiantsGames()
 
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    const mlbURL = 'https://www.baseball-reference.com/leagues/majors/2022-schedule.shtml'
-    const giantsGames =[]
-
-    fetch(mlbURL, )
-    .then(response => response.text())
-    .then(html => {
-        var $ = cheerio.load(html)
-            $('.game').each(function(i,element) {
-            
-                let game = $(element).text().replace(/\s\s+/g, '')
-                let date = $(element).parent().find('h3').text()
-                if(game.includes('San Francisco Giants')){
-                    giantsGames.push([date, game])
-                }           
-            })
-            console.log(giantsGames)
-    })
-
+   
     
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
