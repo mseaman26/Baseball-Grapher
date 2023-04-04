bd9779b74747c54bea25fb5851a2f2a1a79db07f
michael seaman <mseaman26@gmail.com>
2023-04-03 22:19:06 -0700

    zero axis is thicker and other updates

--- client/src/components/LineGraph.js
+++ client/src/components/LineGraph.js
@@ -3,22 +3,24 @@ import Chart from 'chart.js/auto';
 
 const LineGraph = () => {
   const chartRef = useRef(null);
+  const myLineChartRef = useRef(null);
   let myLineChart;
 
   useEffect(() => {
     const ctx = chartRef.current.getContext('2d');
+    
 
-    myLineChart = new Chart(ctx, {
+    myLineChartRef.current = new Chart(ctx, {
       type: 'line',
       data: {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [
           {
             label: 'My First Dataset',
-            data: [65, 59, 80, 81, 56, 55, 40],
+            data: [65, 59, 80, 81, -56, 55, 40],
             fill: false,
             borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1,
+            tension: 0,
           },
         ],
       },
@@ -26,6 +28,17 @@ const LineGraph = () => {
         scales: {
           y: {
             beginAtZero: true,
+            grid: {
+              borderWidth: (context) => {
+                return context.tick.value === 0 ? 2 : 1;
+              },
+              borderColor: (context) => {
+                return context.tick.value === 0 ? 'black' : 'rgba(0, 0, 0, 0.1)';
+              },
+              color: (context) => {
+                return context.tick.value === 0 ? 'black' : 'rgba(0, 0, 0, 0.1)';
+              },
+            },
           },
         },
         onClick: (event, chartElement) => {
@@ -40,7 +53,7 @@ const LineGraph = () => {
     });
 
     return () => {
-      myLineChart.destroy();
+      myLineChartRef.current.destroy();
     };
   }, []);
 
